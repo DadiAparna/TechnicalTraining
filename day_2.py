12aa4397d622bc1bcbec871c8c4db3ae6841c800
Dadi Aparna <111778445+DadiAparna@users.noreply.github.com>
2023-02-02 20:45:30 +0530

    first commit

---
+++ day_2.py
@@ -0,0 +1,49 @@
+#== comparing value and datatype
+a=6
+b=True
+if(1==b):
+    print("yes")
+else:
+    print("no")
+
+#reverse order of alphabets
+    
+for i in range(122,96,-1):
+    print(chr(i),end=" ")
+
+#double for loop
+
+rows = 6
+for i in range(rows):
+    for j in range(i):
+        print(i, end=' ')
+    print('')
+
+        
+#for loop
+
+age=[2,4,6,8]
+for i in range(len(age)):
+    print(i)
+
+#double for
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print("")
+
+
+
+
+    
+#while loop
+
+i = 1
+n = 5
+while i <= n:
+    print(i)
+    i = i + 1
+
+    
+
+
